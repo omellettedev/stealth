75ab9e3fcd54bd2ed2eea160272b912159339ba5
Alexander Pham <108191659+pham-t-alex@users.noreply.github.com>
2025-06-16 23:34:55 -0700

    crouch

---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,75 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// ----- BASED ON STARTER ASSET StarterAssetsInputs.cs -----
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerInputHandler : MonoBehaviour
+{
+    private Vector2 movement;
+    public Vector2 Movement => movement;
+
+    private Vector2 look;
+    public Vector2 Look => look;
+
+    private bool jump;
+    public bool Jump
+    {
+        get { return jump; } 
+        set { jump = value; }
+    }
+
+    private bool sprint;
+    // crouching prevents sprinting
+    public bool Sprint => sprint && !crouch;
+
+    private bool crouch;
+    public bool Crouch => crouch;
+
+    public event Action CrouchEvent;
+    public event Action UncrouchEvent;
+
+    public void HandleMove(InputAction.CallbackContext ctx)
+    {
+        movement = ctx.ReadValue<Vector2>();
+    }
+
+    public void HandleLook(InputAction.CallbackContext ctx)
+    {
+        look = ctx.ReadValue<Vector2>();
+    }
+
+    public void HandleJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started) jump = true;
+        else if (ctx.canceled) jump = false;
+    }
+
+    public void HandleSprint(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            sprint = true;
+        }
+        else if (ctx.canceled) sprint = false;
+    }
+
+    public void HandleCrouch(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            crouch = true;
+            CrouchEvent?.Invoke();
+        }
+        else if (ctx.canceled)
+        {
+            crouch = false;
+            UncrouchEvent?.Invoke();
+        }
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
